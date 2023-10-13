a52912588b5567eaefb1e5269181f82849bbed8e
Johnson Shogbaike <G00389668@atu.ie>
2023-10-13 22:18:01 +0100

    Exercise 1 & 2

---
+++ src/main/java/ie/atu/lab3_userservice/EmailService.java
@@ -0,0 +1,9 @@
+package ie.atu.lab3_userservice;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    public void sendEmail(String to, String message){}
+
+}
