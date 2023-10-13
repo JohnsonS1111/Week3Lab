692afda7902c32c0dd265d7ae753838af3028105
Johnson Shogbaike <G00389668@atu.ie>
2023-10-13 17:55:07 +0100

    User Service Application created

---
+++ src/test/java/ie/atu/lab3_userservice/Lab3UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.lab3_userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab3UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
